e6e50d82ba7933458ce2f2e22d662089ba90f773
naesheim <hg.nesheim@gmail.com>
2017-06-07 10:19:42 +0200

    gitCommit method

--- vars/gitCommit.groovy
+++ vars/gitCommit.groovy
@@ -1,4 +1,13 @@
 def call(){
-	def commit = sh(returnStdout: true,  script: 'git log --online').trim()
-    echo ${commit}
+    def keywords = ['wi', 'work-item',]
+	def commit = sh(returnStdout: true,  script: 'git log -1 --pretty=%B').trim().toLower()
+
+    def workItemList = []
+
+    String keys = "|".join(keywords).toLower()
+    def finder = (commit =~ /($keys)\s+\d+/i)
+    if (finder.size() != 0) {
+        workItemList.add((finder.getAt(0)))
+    }
+    println workItemList
 }
