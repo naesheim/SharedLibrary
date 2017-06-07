9df6797a53a3456bfd47955000e317acf12cbf2c
naesheim <hg.nesheim@gmail.com>
2017-06-07 15:25:17 +0200

    new style

--- vars/gitCommit.groovy
+++ vars/gitCommit.groovy
@@ -1,14 +1,6 @@
 @NonCPS
-def call(){
-    def keywords = ['wi', 'work-item']
-	def commit = sh(returnStdout: true,  script: 'git log -1 --pretty=%B').trim().toLowerCase()
-    String keys = keywords.join(keywords).toLowerCase()
-
-    commit.eachline { line ->
-        def finder = (line =~ /($keys)\s+\d+/)
-        finder.each {
-            println(it.toString())
-        }
-    }
+def call(String data = "wi 492340"){
+    def finder = (data =~ /[0-9]{6}/)
+    echo finder.getAt(0)
 
 }
