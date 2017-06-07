c4d5ae8ce828cffd2dd80287392eac57e42f6aa2
naesheim <hg.nesheim@gmail.com>
2017-06-07 11:43:18 +0200

    eachline

--- vars/gitCommit.groovy
+++ vars/gitCommit.groovy
@@ -1,15 +1,14 @@
+@NonCPS
 def call(){
     def keywords = ['wi', 'work-item']
 	def commit = sh(returnStdout: true,  script: 'git log -1 --pretty=%B').trim().toLowerCase()
+    String keys = keywords.join(keywords).toLowerCase()
 
-    def workItemList = []
+    commit.eachline { line ->
+        def finder = (line =~ /($keys)\s+\d+/)
+        finder.each {
+            println(it.toString())
+        }
+    }
 
-    String keys = "|".join(keywords).toLower()
-    def finder = (commit =~ /($keys)\s+\d+/)
-    if (finder.size() != 0) {
-        println(finder.getAt(0))
-    }
-    else{
-        println("no work item found")
-    }
 }
