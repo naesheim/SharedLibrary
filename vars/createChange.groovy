a8fbe2b1d771a237f167dbe5da331d35a3208e3c
naesheim <hg.nesheim@gmail.com>
2017-06-13 15:42:38 +0200

    tezting

--- vars/createChange.groovy
+++ vars/createChange.groovy
@@ -1,21 +1,25 @@
 def call(String applicationName){
     WORK_ITEM = null
     APPLICATION_NAME = applicationName
-    CHANGE_REPO_URL = "https://github.com/naesheim/chachachanges"
+    CHANGE_REPO_URL = "git@github.com:naesheim/chachachanges.git"
 
-    node {
+    node('master') {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'pythonpass',
+        usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            stage('extract work items') {
+                echo "uname $USERNAME pwd=$PASSWORD"
+                checkout scm
+                GIT_COMMIT = sh(returnStdout: true, script: 'git log -1 --pretty=%B')
+                WORK_ITEM = workItem(GIT_COMMIT)
+            }
 
-        stage('extract work items') {
-            checkout scm
-            GIT_COMMIT = sh(returnStdout: true, script: 'git log -1 --pretty=%B')
-            WORK_ITEM = gitCommit GIT_COMMIT
-        }
-
-        stage('commit change') {
-            git url CHANGE_REPO_URL
-            sh 'pyvenv venv'
-            sh "venv/bin/pip3 install -r requirements.txt"
-            echo sh(returnStdout: true, script: 'ls')
+            stage('commit change') {
+                git branch: 'master', credentialsId: '2d7f1202-2262-4dc2-b9f8-30744ba7688d', url: CHANGE_REPO_URL
+                sh 'python3 -m venv venv'
+                sh "venv/bin/pip3 install -r requirements.txt"
+                echo "work iteks: $WORK_ITEM and applicationName: $APPLICATION_NAME"
+                sh "venv/bin/python3 changeAnywhere -u ${env.USERNAME} -p ${env.PASSWORD}"
+            }
         }
     }
 }
--- vars/workItem.groovy
+++ vars/workItem.groovy
@@ -1,6 +1,6 @@
 @NonCPS
 def call(String data = "wi 492340"){
-    def finder = (data =~ /[0-9]{5}/)
+    def finder = (data =~ /(wi|work-item)\s+\d{5}/)
     def arr = []
     finder.each { number ->
         arr.add(number)
